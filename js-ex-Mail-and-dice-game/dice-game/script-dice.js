a9865ff11d2f9a79fe0e7c48a3cb757c7a08c9e3
Laassel <lassel.giovanni@gmail.com>
2024-10-17 20:06:58 +0200

    Score comparison by winner using the method if else

--- js-ex-Mail-and-dice-game/dice-game/script-dice.js
+++ js-ex-Mail-and-dice-game/dice-game/script-dice.js
@@ -10,3 +10,17 @@ console.log(playerScore)
 const computerScore = Math.floor(Math.random() * 6) +1;
 console.log(computerScore);
 
+// Confronto i due numeri 
+// Se il numero di playerScore e maggiore di quello di computerScore, playerScore vince.
+// Se il numero di computerScore e maggiore di quello di playerScore, computerScore vince.
+// Se i numeri sono uguali e un pareggio.
+
+if (playerScore > computerScore) {
+    console.log(`Hai vinto! Giocatore: ${playerScore}, Computer ${computerScore}`);
+} else if (playerScore < computerScore) {
+    console.log(`Hai perso! Giocatore: ${playerScore}, Computer ${computerScore}`);
+} else {
+    console.log(`E' un pareggio! Giocatore: ${playerScore}, Computer ${computerScore}`);
+
+}
+
