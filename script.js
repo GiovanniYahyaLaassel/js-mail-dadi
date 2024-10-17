e0016b8367f2585c9594ace7e1e7166ae5949de8
Laassel <lassel.giovanni@gmail.com>
2024-10-17 18:33:07 +0200

    Add-file-html-js and Add array with emails

---
+++ script.js
@@ -0,0 +1,15 @@
+//EX-MAIL
+
+//Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+//stampa un messaggio appropriato sull’esito del controllo.
+
+//Inizializzo un array con la lista delle email consentite
+const userEmails = [
+    'm.rossi@mail.com', 
+    'l.bianchi@example.com', 
+    'g.verdi@mail.com',
+    'a.neri@example.com',
+    's.martini@mail.com' 
+];
+
+//
